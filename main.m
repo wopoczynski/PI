65cd979aaadc798d52c91ee4d1c465a17df652b8
Wojtek <wojtek.opoczynski@gmail.com>
2017-12-06 00:16:40 +0100

    add plots, results transfer from GPU, fix mask

--- main.m
+++ main.m
@@ -9,8 +9,9 @@ clear; clc; close all;
 image = imread('airplane.jpg');
 
 % create mask 
+ % jak sie da za duza maske to leci w kosmos na gpu
 mask = false(size(image,1),size(image,2));
-mask(70:250, 80:250) = true;
+mask(140:220, 140:220) = true;
 %% for large images rescale
 % image = imresize(image,.5);
 % mask = imresize(mask,.5);  
@@ -21,7 +22,7 @@ subplot(2,2,2); imshow(mask); title('Maska');
 subplot(2,2,3); title('Segmentacja');
 
 %% params 
-iterations = 1000;
+iterations = 600;
 radius = 50;
 smooth = 0.2;
 display = true;
@@ -29,16 +30,51 @@ threheads = 4;
 parameters = struct('image',image,'initMask',mask,'maxIterations',...
     iterations,'radius', radius,'smooth', smooth,'display', display,...
    'threheads', threheads);
-%% wyniki
+
+
 result = localizedSeg(parameters);
-subplot(2,2,4); imshow(result); title('Wynik');
-
 resultCPU = localizedSegParallel(parameters);  
-subplot(2,2,4); imshow(resultCPU); title('Wynik');
-
 resultGPU = localizedSegParallelGPU(parameters);
-subplot(2,2,4); imshow(resultGPU); title('Wynik');
+imageOutGpu = gather(resultGPU);
+clear resultGPU;
 
+%% wyniki
+figure(1);
+subplot(2,2,1);
+imshow(image);
+title('Input');
+subplot(2,2,2);
+imshow(mask);
+title('Maska');
+subplot(2,2,3);
+title('Segmentacja');
+subplot(2,2,4);
+imshow(result);
+title('Wynik CPU');
 
+figure(2);
+subplot(2,2,1);
+imshow(image);
+title('Input');
+subplot(2,2,2);
+imshow(mask);
+title('Maska');
+subplot(2,2,3);
+title('Segmentacja');
+subplot(2,2,4);
+imshow(resultCPU);
+title('Wynik CPU multithread.');
+
+figure(3);
+subplot(2,2,1);
+imshow(image);
+title('Input');
+subplot(2,2,2);
+imshow(mask);
+title('Maska');
+subplot(2,2,3);
+title('Segmentacja');
+subplot(2,2,4); imshow(imageOutGpu);
+title('Wynik GPU');
 
 
