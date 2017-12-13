c81f708ba7f8dc91225d29a13f04d05939442a79
Wojtek <wojtek.opoczynski@gmail.com>
2017-12-13 19:06:20 +0100

    rename test script and minor changes

--- singlethread.m
+++ singlethread.m
@@ -14,7 +14,8 @@ mask01 = imresize(mask,.1);
 parameters = struct('image',image01,'initMask',mask01,'display', display,'maxIterations',iterations);
 tic;
 result01 = localizedSeg(parameters);
-timeCPU01 = toc/60;
+timeCPU01 = toc;
+disp('done 0.1');
 
 %% 0.25
 image025 = imresize(image,.25);
@@ -22,7 +23,8 @@ mask025 = imresize(mask,.25);
 parameters = struct('image',image025,'initMask',mask025,'display', display,'maxIterations',iterations);
 tic;
 result025 = localizedSeg(parameters);
-timeCPU025 = toc/60;
+timeCPU025 = toc;
+disp('done .2');
 
 %% 0.5
 image05 = imresize(image,.5);
@@ -30,8 +32,9 @@ mask05 = imresize(mask,.5);
 parameters = struct('image',image05,'initMask',mask05,'display', display,'maxIterations',iterations);
 tic;
 result05 = localizedSeg(parameters);
-timeCPU05 = toc/60;
+timeCPU05 = toc;
 
+disp('done .5');
 
 %% 0.75
 image075 = imresize(image,.75);
@@ -39,14 +42,16 @@ mask075 = imresize(mask,.75);
 parameters = struct('image',image075,'initMask',mask075,'display', display,'maxIterations',iterations);
 tic;
 result075 = localizedSeg(parameters);
-timeCPU075 = toc/60;
+timeCPU075 = toc;
+disp('done .75');
 
 %% 1
 parameters = struct('image',image,'initMask',mask,'maxIterations',iterations,'display',display);
 
 tic;
 result1 = localizedSeg(parameters);
-timeCPU1 = toc/60;
+timeCPU1 = toc;
+disp('done1');
 
 %% 1.5
 
@@ -55,7 +60,8 @@ mask15 = imresize(mask,1.5);
 parameters = struct('image',image15,'initMask',mask15,'display', display,'maxIterations',iterations);
 tic;
 result15 = localizedSeg(parameters);
-timeCPU15 = toc/60;
+timeCPU15 = toc;
+disp('done 1,5');
 
 %% 2
 
@@ -64,8 +70,9 @@ mask2 = imresize(mask,2);
 parameters = struct('image',image2,'initMask',mask2,'display', display,'maxIterations',iterations);
 tic;
 result2 = localizedSeg(parameters);
-timeCPU2 = toc/60;
+timeCPU2 = toc;
 
+disp('done2');
 
 %% 4
 
@@ -74,7 +81,7 @@ mask4 = imresize(mask,4);
 parameters = struct('image',image4,'initMask',mask4,'display', display,'maxIterations',iterations);
 tic;
 result4 = localizedSeg(parameters);
-timeCPU4 = toc/60;
+timeCPU4 = toc;
 
 
 x = [.1 .25 .5 .75 1 1.5 2 4];
@@ -82,7 +89,7 @@ y = [timeCPU01 timeCPU025 timeCPU05 timeCPU075 timeCPU1 timeCPU15 timeCPU2 timeC
 figure(1);
 plot(x,y);
 xlabel('size');
-ylabel('time [s]');
+ylabel('time [min]');
 title('processing time 1 thread');
 
 save('timingCore');
