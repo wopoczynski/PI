2a482ffcec44c0324dbf9fd852bb8e91f33a22cf
Wojtek <wojtek.opoczynski@gmail.com>
2017-12-17 12:48:43 +0100

    cpu threads test

---
+++ cluster_multiThreadsCheck.m
@@ -0,0 +1,23 @@
+%%%% CPU threads check
+image = imread('airplane.jpg');
+
+mask = false(size(image,1),size(image,2));
+mask(140:220, 140:220) = true; 
+
+iterations = 600; %iteracje
+display = false; % T/F czy ma wyœwietlaæ przebieg
+threads = [1, 2, 4, 8, 12, 16, 32, 48, 64, 72, 96]; % ilosc watkow taka?
+
+for i = 1:numel(threads)
+    
+    imageTmp = imresize(image,8);
+    maskTmp = imresize(mask,8);  
+    parameters = struct('image',imageTmp,'initMask',maskTmp,'display', display,'maxIterations',iterations, 'threads', threads(i));
+    tic;
+    result(i) = localizedSegParallel(parameters);
+    timesMultiThreads(i) = toc;
+    disp(['done ' num2str(threads(i))]);
+end
+
+save('resultsMultiCpuTest');
+disp('alldone');
